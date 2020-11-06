3d72dd4dddc6b17c50249dce137ab5a8685ebc56
Ankit Kumar <ankit3612kumar@gmail.com>
2020-11-06 23:49:11 +0530

    reverse string without reversing words in it

---
+++ Strings/reverseStringNotWords.cpp
@@ -0,0 +1,45 @@
+// Problem link- https://practice.geeksforgeeks.org/problems/reverse-words-in-a-given-string/0
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+
+    // 't' is no. of test-cases.
+    while (t--)
+    {
+        string s;
+        string str = "", result = "";
+        cin >> s;
+        int len = s.size();
+
+        // Traversing string from back and concatinating alphabets until a dot is found.
+        for (int i = len - 1; i >= 0; i--)
+        {
+            if (s[i] != '.')
+            {
+                str += s[i];
+            }
+
+            // As soon as we encounter a dot, reverse it to make it in original form and push it into result.
+            else if (s[i] == '.')
+            {
+                reverse(str.begin(), str.end());
+                result += str;
+                result += ".";
+                str = "";
+            }
+        }
+
+        // To reverse last word as we won't encounter dot for it.
+        reverse(str.begin(), str.end());
+        result += str;
+        cout << result << endl;
+    }
+    return 0;
+}
