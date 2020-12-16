c93ca81fdd4be793287de5f2594f27da60c79a32
Ankit Kumar <ankit02111997@gmail.com>
2020-12-17 00:10:22 +0530

    one bug solved

--- Recursion/subsetSum.cpp
+++ Recursion/subsetSum.cpp
@@ -6,7 +6,6 @@ using namespace std;
 class Solution
 {
 public:
-
     // To find subset with given sum, returns 1 or 0.
     int isSubsetSum(int N, int arr[], int sum)
     {
@@ -14,7 +13,10 @@ public:
             return 1;
         if (N == 0 || sum == 0)
             return 0;
-        return isSubsetSum(N - 1, arr, sum) || isSubsetSum(N - 1, arr, sum - arr[N - 1]);
+        if (sum > arr[N - 1])
+            return isSubsetSum(N - 1, arr, sum) || isSubsetSum(N - 1, arr, sum - arr[N - 1]);
+        else
+            return isSubsetSum(N - 1, arr, sum);
     }
 
     // To calculate half of total sum of elements of array cz we need to find if any subset exists with that sum(above func does that). Other half will automatically be equal.
